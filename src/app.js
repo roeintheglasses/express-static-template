d36437ad1a9998a24da55605e102b984f383a6c3
roeintheglasses <hrkjangir@gmail.com>
2020-08-04 18:20:32 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//imports
+const express = require("express");
+const path = require("path");
+
+//Path and Env variables Setup
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+//Express consts
+const app = express();
+
+// Express & ejs Setup
+app.use(express.static(publicDirectoryPath));
+
+//Bodyparser
+app.use(express.urlencoded({
+    extended: false
+}));
+
+//Server start
+app.listen(port, console.log("Server running at port " + port));
